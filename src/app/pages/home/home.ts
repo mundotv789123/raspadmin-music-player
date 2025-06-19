6bef4d1ccd250395945a7f3b68a078c00dd2fbdd
Michael Fernandes <789michaelfernandes789@gmail.com>
2025-06-18 23:13:29 -0300

    copiando estrutura de player de audio do raspadmin react

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBackwardFast, faBars, faEyeSlash, faForwardFast, faPause, faShuffle, faVolumeHigh, faVolumeMute, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class HomeComponent {
-
+  faVolumeMute=faVolumeMute
+  faBackwardFast=faBackwardFast
+  faPause=faPause
+  faForwardFast=faForwardFast
+  faShuffle=faShuffle
+  faVolumeHigh=faVolumeHigh
+  faEyeSlash=faEyeSlash
+  faBars=faBars
+  faXmark=faXmark
 }
