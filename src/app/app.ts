5c424b5a1bda70350e8be038aaf654093b910270
Michael Fernandes <789michaelfernandes789@gmail.com>
2025-06-18 22:55:25 -0300

    init tailwindcss

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: '<router-outlet/>',
 })
-export class App {
-  protected title = 'raspadmin-music-player';
-}
+export class App { }
